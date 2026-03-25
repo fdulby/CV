a14a84c0c046a4c2f95134a4d1e8db0a27a40ba0
Liu bingyi <835783858@qq.com>
2026-03-25 20:43:29 +0800

    2016论文的U-net复现版本--02，batch=128，lr=1e-4

--- 2016.py
+++ 2016.py
@@ -44,8 +44,8 @@ CFG = {
     "test_ratio": 0.1,
 
     "image_size": 224,
-    "batch_size": 96,  # 双卡 5090 显存充足，建议 64 或 128
-    "num_workers": 8,
+    "batch_size": 128,  # 双卡 5090 显存充足，建议 64 或 128
+    "num_workers": 24,
     "pin_memory": True,
 
     "num_color_bins": 313,
@@ -61,7 +61,7 @@ CFG = {
 
     "device": "cuda" if torch.cuda.is_available() else "cpu",
     "epochs": 50,
-    "lr": 3e-5,
+    "lr": 1e-4,
     "betas": (0.9, 0.99),
     "weight_decay": 1e-3,
 
